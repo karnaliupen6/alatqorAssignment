c6c99171ff3400a83fb1faa31f18a97e71e77635
karnaliupen6 <Karnaliupen6@gmail.com>
2023-05-12 08:43:20 -0400

    question1

---
+++ src/altaqor/assignments/assignment14/Question1.java
@@ -0,0 +1,19 @@
+package altaqor.assignments.assignment14;
+
+public class Question1 {
+
+	public static void main(String[] args) {
+		 int i = 0;	
+		 
+		 while(i<10) {
+			 System.out.println("upendra, Nepal!");
+			 i++;
+		 }
+	}
+	
+	 
+		
+	
+		
+
+}
